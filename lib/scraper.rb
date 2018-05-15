39c4d5bef6504d4474b6cc976177139bf076fcc2
Adrienne Yao <adrienne.yao@icloud.com>
2018-05-15 20:03:47 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,10 +21,10 @@ class Scraper
 
    def make_courses
      self.get_courses.each do |post|
-    course = Course.new
-    course.title = post.css("h2").text
-    course.schedule = post.css(".date").text
-    course.description = post.css("p").text
+     course = Course.new
+     course.title = post.css("h2").text
+     course.schedule = post.css(".date").text
+     course.description = post.css("p").text
   end
 
     def print_courses
